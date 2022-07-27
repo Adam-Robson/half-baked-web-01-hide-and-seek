729c0c661561119ead15051c1990cce201bf350b
Adam Robson <adamray312@gmail.com>
2022-07-27 03:39:16 -0700

    add styles function and fix the reset button

--- app.js
+++ app.js
@@ -6,30 +6,29 @@ const boulderButton = document.getElementById('boulder-button');
 const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
 const boulderContainer = document.getElementById('boulder-container');
-const totalEl = document.getElementById('total');
-const lossesEl = document.getElementById('losses');
-const winsEl = document.getElementById('wins');
+let totalEl = document.getElementById('total');
+let lossesEl = document.getElementById('losses');
+let winsEl = document.getElementById('wins');
 const resetNow = document.getElementById('reset-now');
 
-
 const hidingPlaces = ['tree', 'shed', 'boulder'];
-
 
 let correctGuesses = 1;
 let totalGuesses = 1;
 let incorrectGuesses = 1;
 
-
 shedButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'shed');
+
 });
 
 treeButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'tree');
+
 });
 
 boulderButton.addEventListener('click', () => {
@@ -38,17 +37,27 @@ boulderButton.addEventListener('click', () => {
     handleGuess(answer, 'boulder');
 });
 
-
 function handleGuess(correctSpot, userGuess) {
-    totalEl.textContent = totalGuesses++;
+    resetStyles();
     const correctEl = document.getElementById(correctSpot + '-container');
     correctEl.classList.add('face');
     if (userGuess === correctSpot) {
-        winsEl.textContent = correctGuesses++;    
+        winsEl.textContent = correctGuesses++;
     } else {
         lossesEl.textContent = incorrectGuesses++;
     }
-    resetNow.addEventListener('click', () => {
-        correctEl.classList.remove('face');
-    });
+    totalEl.textContent = totalGuesses++;
 }
+
+function resetStyles() {
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    shedContainer.classList.remove('face');
+}
+
+resetNow.addEventListener('click', () => {
+    resetStyles();
+    winsEl.textContent = 0;
+    totalEl.textContent = 0;
+    lossesEl.textContent = 0;
+});
